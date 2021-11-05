5f2fd98bcdf5a4a9c328e452c0f332889ce01ead
Sviatoslav-Valentyn <Sviatoslav-Valentyn.Hvozdyk.IT.2021@lpnu.ua>
2021-11-05 14:02:59 +0200

    first

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab 6.1/Source.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			int A[] = { 0, 4, -1, -2 };
+			int S = Sum(A, 4);
+			Assert::AreEqual(1, S);
+		}
+	};
+}
